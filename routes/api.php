85c71eba2b4b17debba4b8ce993860f50885d22e
Farrelka <famaadh3@gmail.com>
2023-02-07 19:31:46 +0700

    feat: resto

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\RestoController;
 
 Route::post('/auth/login', [AuthenticationController::class, 'login'])->name('auth.login');
 Route::post('/auth/register', [AuthenticationController::class, 'register'])->name('auth.register');
@@ -10,3 +11,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/auth/profile', [AuthenticationController::class, 'profile'])->name('auth.profile');
     Route::get('/auth/logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
 });
+
+Route::apiResource('restos', RestoController::class);
